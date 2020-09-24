1ee4ee6ddc278c4bf04fd70f53aa60fc4a538895
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-24 11:25:04 +0200

    Adding static methods get and getCurrentSpawn in class SpawnPlugin

--- src/main/java/fr/pederobien/minecraftspawn/SpawnPlugin.java
+++ src/main/java/fr/pederobien/minecraftspawn/SpawnPlugin.java
@@ -3,19 +3,38 @@ package fr.pederobien.minecraftspawn;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.dictionary.interfaces.IDictionaryParser;
+import fr.pederobien.minecraftgameplateform.interfaces.commands.IParentCommand;
 import fr.pederobien.minecraftgameplateform.utils.Plateform;
+import fr.pederobien.minecraftspawn.interfaces.ISpawn;
 
 public class SpawnPlugin extends JavaPlugin {
-	public static final String NAME = "minecraft-spawn";
+	private static Plugin plugin;
+	private static IParentCommand<ISpawn> spawnCommand;
+
+	/**
+	 * @return The plugin associated to this spawn plugin.
+	 */
+	public static Plugin get() {
+		return plugin;
+	}
+
+	/**
+	 * @return The current spawn for this plugin.
+	 */
+	public static ISpawn getCurrentSpawn() {
+		return spawnCommand.getParent().get();
+	}
 
 	@Override
 	public void onEnable() {
 		Plateform.getPluginHelper().register(this);
+		plugin = this;
 
-		new SpawnCommand(this);
+		spawnCommand = new SpawnCommand(this);
 
 		registerDictionaries();
 	}
@@ -29,7 +48,7 @@ public class SpawnPlugin extends JavaPlugin {
 	}
 
 	private void registerDictionary(String parent, String... dictionaryNames) {
-		Path jarPath = Plateform.ROOT.getParent().resolve(NAME.concat(".jar"));
+		Path jarPath = Plateform.ROOT.getParent().resolve(getName().concat(".jar"));
 		String dictionariesFolder = "resources/dictionaries/".concat(parent).concat("/");
 		for (String name : dictionaryNames)
 			registerDictionary(Plateform.getDefaultDictionaryParser(dictionariesFolder.concat(name)), jarPath);
